a3bd59d32664dff84704631bb91433a8710a8ee6
ashishkirtwed <=>
2021-12-27 19:39:01 +0530

    Creates Stack Folder

---
+++ Stack/From Scratch/Stack.java
@@ -0,0 +1,19 @@
+class Stack {
+	Stack(){
+	int MAX = 1000;
+	int arr[] = new int[MAX];
+	int top=-1;
+	}
+	public boolean isEmpty(){
+		return (top<0);		
+	}
+	public boolean isFull(){
+		return (top==MAX-1);
+	}
+	public 
+	public static void main(String args[]){
+		
+	}
+		
+		
+		
