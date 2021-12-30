20f789aceb0d632b847674b370726f5e04820934
ashishkirtwed <=>
2021-12-30 23:06:46 +0530

    Modifies the code

--- Graph/Graph Implementation/Undirect Graph/GraphImplementation.java
+++ Graph/Graph Implementation/Undirect Graph/GraphImplementation.java
@@ -1,4 +1,5 @@
 /* Implementation of undirected graph */
+import java.util.Scanner;
 import java.util.*;
 class Graph{
 	//adding an edge to the graph
@@ -10,4 +11,22 @@ class Graph{
 		for(int i =0;i<g.size();i++){
 			System.out.println("\n Adjacency list of vertex 
 			for(j=0;j<g.get(i).size();j++){
-				
+				System.out.println("--> "+g.get(i).get(j));
+			}
+		}
+	}
+}
+
+class GraphImplementation{
+	public static void main(String args[]){
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Please input no of vertex: ");
+		int v =sc.nextInt();
+		System.out.println("Please input source and dest");
+		Graph g = new Graph();
+		while(true){
+			int source  =sc.nextInt();
+			int dest = sc.nextInt();
+			System.out.println("Do you want to continue ");
+			String opt = 
+			
